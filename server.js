3b9565357d0fbe2a4ecd20819db7af606c355a2e
Marc Hudak <Marc.Hudak@gmail.com>
2015-04-07 23:31:31 -0500

    testing static fileserve

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(express.static('CometRadarAdmin'));
   	
 // });
 
-var server = app.listen(3000, function () {
+app.listen(3000, function () {
 
   var host = server.address().address;
   var port = server.address().port;
