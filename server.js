69f7b9e59857f2c26a061d950a6b2c0f9f3ed896
Marc Hudak <Marc.Hudak@gmail.com>
2015-04-08 04:31:33 -0500

    testing static fileserve

--- server.js
+++ server.js
@@ -43,6 +43,7 @@ var server = app.listen(3000, function () {
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
+  console.log(__dirname + '/CometRadarAdmin');
 
 });
 
