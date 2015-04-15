8f4557979933e4802dedd79936b2677311d26500
Marc Hudak <Marc.Hudak@gmail.com>
2015-04-15 00:49:29 -0500

    fixing sql queries

--- server.js
+++ server.js
@@ -145,7 +145,7 @@ app.get('/api/getInfo', function (req, res) {
 //TODO move behind login wall
 app.get('/api/getRiderLocations', function (req, res) {
   //TODO add date to query
-  connection.query('SELECT * FROM `pickup_request` WHERE ROUTE_NAME=\'' + req.query.rname + '\'', function (error, results, fields) {
+  connection.query('SELECT lat, long FROM `pickup_request` WHERE ROUTE_NAME=\'' + req.query.rname + '\'', function (error, results, fields) {
   // error will be an Error if one occurred during the query
   // results will contain the results of the query
   // fields will contain information about the returned results fields (if any)
